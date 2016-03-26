4ee9e27f7fe1101d5ee700d7f3a77f864d39b97b
kortirso <kortirso@gmail.com>
2016-03-26 15:50:48 +0300

    prepare to deploy

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,6 @@ class User < ActiveRecord::Base
     devise :database_authenticatable, :registerable, :rememberable, :trackable
 
     has_many :games
+
+    validates :username, presence: true, uniqueness: true, length: { in: 1..20 }
 end
